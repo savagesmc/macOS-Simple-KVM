62cddf6938d502ca534573dafa01aef5fad80837
Clark, Steve <steve@clarkhome.org>
2019-10-16 12:18:00 -0500

    Updates for my personal Install.

--- basic.sh
+++ basic.sh
@@ -8,9 +8,9 @@ OVMF=$VMDIR/firmware
 
 qemu-system-x86_64 \
     -enable-kvm \
-    -m 2G \
+    -m 32G \
     -machine q35,accel=kvm \
-    -smp 4,cores=2 \
+    -smp cpus=16,cores=8,threads=2,sockets=1 \
     -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
     -device isa-applesmc,osk="$OSK" \
     -smbios type=2 \
@@ -20,9 +20,11 @@ qemu-system-x86_64 \
     -device ich9-intel-hda -device hda-output \
     -usb -device usb-kbd -device usb-mouse \
     -netdev user,id=net0 \
-    -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+    -device e1000-82545em,netdev=net0,id=net0,mac=64:f7:c6:6a:85:49 \
     -device ich9-ahci,id=sata \
     -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
     -device ide-hd,bus=sata.2,drive=ESP \
     -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
     -device ide-hd,bus=sata.3,drive=InstallMedia \
+    -drive id=SystemDisk,if=none,file=MyDisk.qcow2 \
+    -device ide-hd,bus=sata.4,drive=SystemDisk \
